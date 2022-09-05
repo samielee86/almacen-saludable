ed2abe71b5c0da9eac8199dbada37e80a9527e4d
Samie Lee <samielee86@gmail.com>
2022-09-05 10:21:27 -0300

    debuggueando entrega rutas

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,14 +1,20 @@
 import React, {useState, useEffect } from 'react'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import products from '../../mock/products'
+import ItemCount from '../itemCount/itemCount'
+import { useParams } from 'react-router-dom'
 
 const ItemDetailContainer = () => {
+
+    const onAdd = (qty) => {
+        alert(`Se agregaron ${qty} productos al carrito`)}
+
     const [item, setItem] = useState({})
+    const idProd = Number(useParams())
 
     useEffect( ()=>{
         const getProduct = () => new Promise((res,rej)=>{
-            const product = products.find( prod => prod.id === 1)
-
+            const product = products.find( prod => prod.id === idProd)
             setTimeout( () => {
                 res(product);
             }, 2000);
@@ -21,11 +27,19 @@ const ItemDetailContainer = () => {
         .catch((error)=>{
             console.log(error)
         })
-    }, [])
+    }, [idProd])
 
     return(
-        <ItemDetail item={item}/>
-    )
-}
+        <>
+            <ItemDetail item={item}/>
+            <ItemCount 
+                stock={10}
+                initial={1}
+                onAdd={onAdd}
+            />
+        </>
+    );
+    
+};
 
 export default ItemDetailContainer;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import { CartWidget } from './CartWidget'
 import styles from './NavBar.module.css'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <>
         <nav className={styles.navContainer} >
-                <h2 className = {styles.navTitle}>ALMACEN SALUDABLE</h2>
+                <Link to='/'><h2 className={styles.navTitle}>ALMACEN SALUDABLE</h2></Link>
                 <ul className= {styles.navUl}>
-                    <li className={styles.navUlLi}>
-                        <a href="https://google.com">PRODUCTOS</a>
-                    </li>
-                    <li className={styles.navUlLi}>
-                        <a href="https://google.com">EL ALMACEN</a>
-                    </li>
-                    <li className={styles.navUlLi}>
-                        <a href="https://google.com">MÁS INFORMACION</a>
-                    </li>
+                        <Link to="/category/aceites" className={styles.navUlLi}>ACEITES</Link>
+                        <Link to="/category/frutos-secos" className={styles.navUlLi}>FRUTOS SECOS</Link>
+                        <Link to="/category/sal" className={styles.navUlLi}>SAL</Link>
+                        <Link to="/category/miel" className={styles.navUlLi}>MIEL</Link>
+                        <Link to="/category/cookies" className={styles.navUlLi}>COOKIES</Link>
+                        <Link to="/category/pastas" className={styles.navUlLi}>PASTAS</Link>
                 </ul>
-                <CartWidget/>
+                <Link to='/cart'> <CartWidget/> </Link>
             </nav>
     </>
     
