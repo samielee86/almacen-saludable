9a358371c1b0e82e62c174be6d50550cbbf36caf
Samie Lee <samielee86@gmail.com>
2022-10-03 18:20:43 -0300

    navbar dinamico

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CartWidget } from './CartWidget'
 import styles from './NavBar.module.css'
 import { Link } from 'react-router-dom'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../../firebaseConfig'
 
 const NavBar = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const catCollection = collection(db, 'categories')
+    getDocs(catCollection)
+    .then((res)=> {
+      const cats = res.docs.map((cat)=>{
+        return {
+          id: cat.id,
+          ...cat.data()
+        }
+      })
+      setCategories(cats);
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }, [])
+
   return (
     <>
         <nav className={styles.navContainer} >
                 <Link to='/' className={styles.navTitle}><h2 >ALMACEN SALUDABLE</h2></Link>
                 <ul className= {styles.navUl}>
-                        <Link to="/category/aceites" className={styles.navUlLi}>ACEITES</Link>
-                        <Link to="/category/frutossecos" className={styles.navUlLi}>FRUTOS SECOS</Link>
-                        <Link to="/category/sal" className={styles.navUlLi}>SAL</Link>
-                        <Link to="/category/miel" className={styles.navUlLi}>MIEL</Link>
-                        <Link to="/category/cookies" className={styles.navUlLi}>COOKIES</Link>
-                        <Link to="/category/pastas" className={styles.navUlLi}>PASTAS</Link>
+                  <>
+                    {
+                      categories.map((cat)=> (
+                        <Link key={cat.route} to={`/category/${cat.route}`} className={styles.navUlLi}>{cat.name}</Link>
+                      ))
+                    }
+                  </>
                 </ul>
                 <Link to='/cart'> <CartWidget/> </Link>
             </nav>
     </>
-    
+
   )
 }
 
